5eb03284b7e31b0a28ea28e2cff8471492d05018
iqbaluken <iqbalsukron@gmail.com>
2021-07-05 04:54:36 +0700

    pertemuan14

---
+++ app/src/main/java/id/co/pelitabangsa/myapplication/ActionBarActivity.java
@@ -0,0 +1,12 @@
+package id.co.pelitabangsa.myapplication;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ActionBarActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+
+    protected boolean onOptionsItemSelected(MenuItem item) {
+    }
+}
